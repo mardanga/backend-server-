ac56e3854e1d3eef79665c415830a4d4cdadd3e7
mardanga <gaitan@CESVIARG>
2018-07-27 10:19:14 -0300

    seccion completa

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 //requieres
 var express = require('express');
+var mongoose = require('mongoose');
 
 //inicializacion variables
 var app = express();
 
+//conexion a BD
+mongoose.connection.openUri('mongodb://localhost:27017/HospitalDB', { useNewUrlParser: true }, (err, resp) => {
+    if (err) throw err;
+
+    console.log('Mongo Database: \x1b[31m%s\x1b[0m', 'online');
+});
+
 //escuchar peticiones
 
 app.listen(3000, () => {
-    console.log('Express puerto 3000: online');
+    console.log('Express puerto 3000: \x1b[31m%s\x1b[0m', 'online');
 });
